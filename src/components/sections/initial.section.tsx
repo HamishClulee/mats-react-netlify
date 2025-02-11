3ee1f450658e828c68d8029a8a91f25e2144d5ab
HamishClulee <hamish.clulee@hotmail.com>
2025-02-11 17:40:27 +0700

    Tweak and improve cv styling and fonts and animations

--- src/components/sections/initial.section.tsx
+++ src/components/sections/initial.section.tsx
@@ -4,7 +4,7 @@ export const InitialSection = () => {
       className="w-screen min-h-screen flex-col items-stretch"
       style={{ background: "#f0db4f" }}
     >
-      <div className="absolute right-10 -bottom-50 flex items-end">
+      <div className="absolute right-10 -bottom-30 flex items-end">
         <h1 className="text-[#3c3a3a] josefin-sans">HC</h1>
       </div>
     </div>
